8e5dc0f4e23d413268c3de29f32f91b5c487dc91
Santxx <sattu.santitt@gmail.com>
2025-05-18 18:38:29 -0300

    Base

--- src/server.js
+++ src/server.js
@@ -5,9 +5,19 @@ const path = require("path")
 const app = express()
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "../public/views"))
+app.use(express.static(path.join(__dirname, "../public/statics")))
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+app.get("/doe", (req, res) => {
+    res.render("doacoes")
+})
+app.get("/patrocine", (req, res) => {
+    res.render("patrocinio")
+})
+app.get("/contato", (req, res) => {
+    res.render("contato")
 })
 
 app.listen(PORT, () => {
